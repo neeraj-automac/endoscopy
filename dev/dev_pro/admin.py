b338b035d0af36c477100f7a54bbc42f7247c4ba
dileep4522 <dileep45229@gmail.com>
2024-12-03 13:41:23 +0530

    add migrations

--- dev/dev_pro/admin.py
+++ dev/dev_pro/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Patientreports,Patientsdetails,UserDetails
 
 # Register your models here.
+admin.site.register(Patientreports)
+admin.site.register(Patientsdetails)
+admin.site.register(UserDetails)
