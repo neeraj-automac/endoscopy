702ca1a7fd0cbe65eb8f15dbc68da29d94de6c8b
neeraj <neeraj.automac@gmail.com>
2024-12-10 17:13:14 +0530

    debug false

--- dev/dev/settings.py
+++ dev/dev/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bbgd1o(&q-t0#-h^*)g6y@j9sy3#7e2=9hvx)c2)oe-*ledu*8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
